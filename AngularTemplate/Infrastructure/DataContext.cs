0b6d067439cbf4f918a84fd1d3c9421ae2652dd0
John Schwartz <SchwartzJohnG@Gmail.com>
2016-02-23 18:58:37 -0600

    Added files via upload

---
+++ AngularTemplate/Infrastructure/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AngularTemplate.Domain.Models;
+using AngularTemplate.Models;
+
+namespace AngularTemplate.Infrastructure {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false) {
+        }
+
+        public static ApplicationDbContext Create() {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ AngularTemplate/Scripts/ngApp/app.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    angular.module("$safeprojectname$", ['ngRoute', 'ui.bootstrap', 'ngResource', 'ngAnimate'])
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+            /*
+                .when('url/:urlParam1', {
+                    templateUrl: '/url/to/template.html',
+                    controller: 'BlahController',
+                    controllerAs: 'vm'
+                })
+            */
+                .otherwise({
+                    templateUrl: '/ngViews/notFound.html'
+                });
+        }]);
+})();
